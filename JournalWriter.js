17b2ff0e30f6a88523695be98446999e3f352f0d
Shippey <BENJAMIN_W_SHIPPEY@homedepot.com>
2019-01-30 20:15:13 -0500

    Modified Program to write transactions to a Ledger MongoDB collection

--- JournalWriter.js
+++ JournalWriter.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+
 /***************************************
 Program Description:
 
@@ -7,16 +9,55 @@ Record transaction as double-entry to ledger
 Present ledger in format familiar to accountants
 ***************************************/
 
-//Defining Program Classes
+/***************************************
+Resources:
+https://mongodb.github.io/node-mongodb-native/
+***************************************/
+
+var Company = function(name, database) {
+  this.name = name;
+  this.ledger = database.getTable('Ledger');
+}
+
 var TransactionType = function(name, debitAccount, creditAccount) {
   this.name = name;
   this.debitAccount = debitAccount.name;
   this.creditAccount = creditAccount.name;
 }
 
-var Company = function(name) {
-  this.name = name;
-  this.ledger = [];
+var Database = function(server_url, databaseName) {
+  this.server_url = server_url
+  this.name = databaseName;
+};
+
+Database.prototype.getTable = function(table) {
+  return new DatabaseTable(this, table);
+}
+
+var DatabaseTable = function(database,tableName) {
+  this.name = tableName;
+  this.database = database;
+};
+
+DatabaseTable.prototype.insert = function(data) {
+  var client = new MongoClient(this.database.server_url);
+  client.connect((err, client) => {
+    var databaseName = this.database.name;
+    var collectionName = this.name;
+    var collection = client.db(databaseName).collection(collectionName);
+    collection.insertOne(data);
+    client.close();
+  })
+}
+
+DatabaseTable.prototype.selectLast = async function() {
+  var client = new MongoClient(this.database.server_url);
+  await client.connect();
+  var databaseName = this.database.name;
+  var collectionName = this.name;
+  var collection = client.db(databaseName).collection(collectionName);
+  console.log(await collection.find({}).sort('_id',-1).limit(1).toArray());
+  await client.close();
 }
 
 var Accountant = function(name, company) {
@@ -29,13 +70,16 @@ var Account = function(name, category) {
   this.category = category;
 }
 
-Accountant.prototype.recordTransaction = function(transactionType, amount) {
+Accountant.prototype.recordTransaction = async function(transactionType, amount) {
   //get current transactionID
-  var lastTransaction = this.company.ledger[this.company.ledger.length - 1];
-  if (lastTransaction != undefined) {
-    var transactionID = lastTransaction.TransactionID + 1;
+  var transactionID;
+  var lastTransaction = await this.company.ledger.selectLast()[0];
+  if (lastTransaction == undefined) {
+    transactionID = 0
+    console.log('Debugging: ' + lastTransaction);
   } else {
-    var transactionID = 1;
+    console.log('Debugging: ' + lastTransaction);
+    transactionID = lastTransaction.TransactionID + 1;
   }
 
   //record transaction in ledger
@@ -43,11 +87,12 @@ Accountant.prototype.recordTransaction = function(transactionType, amount) {
     {'debit':{'TransactionID':transactionID, 'Account':transactionType.debitAccount, 'Change':'Debit','Amount':amount}
       ,'credit':{'TransactionID':transactionID, 'Account':transactionType.creditAccount, 'Change':'Credit', 'Amount':amount}};
 
-  this.company.ledger.push(transaction.debit);
-  this.company.ledger.push(transaction.credit);
+  this.company.ledger.insert(transaction.debit);
+  this.company.ledger.insert(transaction.credit);
 }
 
 module.exports.TransactionType = TransactionType;
 module.exports.Company = Company;
 module.exports.Accountant = Accountant;
 module.exports.Account = Account;
+module.exports.Database = Database;
--- JournalWriterClient.js
+++ JournalWriterClient.js
@@ -1,30 +1,37 @@
-var readline = require('readline-sync');
-var journalWriter = require('./JournalWriter.js');
+var main = async function() {
 
-var TransactionType = journalWriter.TransactionType;
-var Company = journalWriter.Company;
-var Accountant = journalWriter.Accountant;
-var Account = journalWriter.Account;
+  var readline = require('readline-sync');
+  var journalWriter = require('./JournalWriter.js');
 
-var cash = new Account('Cash','Assets');
-var inventory = new Account('Inventory', 'Assets')
-var ownersEquity = new Account('Owner\'s Equity','Owner\'s Equity');
-var accountsPayable = new Account('Accounts Payable', 'Liabilities');
-var loansPayable = new Account('Loans Payable', 'Liabilities');
-var sales = new Account('Sales', 'Revenue')
+  var TransactionType = journalWriter.TransactionType;
+  var Company = journalWriter.Company;
+  var Accountant = journalWriter.Accountant;
+  var Account = journalWriter.Account;
+  var Database = journalWriter.Database;
+
+  var cash = new Account('Cash','Assets');
+  var inventory = new Account('Inventory', 'Assets')
+  var ownersEquity = new Account('Owner\'s Equity','Owner\'s Equity');
+  var accountsPayable = new Account('Accounts Payable', 'Liabilities');
+  var loansPayable = new Account('Loans Payable', 'Liabilities');
+  var sales = new Account('Sales', 'Revenue')
 
 
-//Main Execution
-var cashSale = new TransactionType('Cash Sale', cash, sales);
-var ownerInvestment = new TransactionType('Owner Investment', cash, ownersEquity);
-var bankLoan = new TransactionType('Bank Loan', cash, loansPayable);
+  //Main Execution
+  var cashSale = new TransactionType('Cash Sale', cash, sales);
+  var ownerInvestment = new TransactionType('Owner Investment', cash, ownersEquity);
+  var bankLoan = new TransactionType('Bank Loan', cash, loansPayable);
 
-var company = new Company('Furniture by Ben');
-var accountant = new Accountant('Ben', company);
+  var server_url = 'mongodb://localhost:27017';
+  var database = new Database(server_url, 'Accounting');
+  var company = new Company('Furniture by Ben', database);
+  var accountant = new Accountant('Ben', company);
 
-accountant.recordTransaction(cashSale, 3);
-accountant.recordTransaction(ownerInvestment, 500);
-accountant.recordTransaction(cashSale, 3);
-accountant.recordTransaction(cashSale, 7);
+  await accountant.recordTransaction(cashSale, 3);
+  await accountant.recordTransaction(ownerInvestment, 500);
+  await accountant.recordTransaction(cashSale, 3);
+  await accountant.recordTransaction(cashSale, 7);
 
-console.log(company.ledger);
+}
+
+main();
